096015dd128542527826551dbaf870433c7359d2
DAISUKE007 <da1suke.haseee@gmail.com>
2020-02-18 20:57:45 +0900

    js修正

--- app/assets/javascripts/hamburger.js
+++ app/assets/javascripts/hamburger.js
@@ -1,16 +1,16 @@
-$(document).on('turbolinks:load', function(){
-  $(function(){
-    $('#menu_link').click(function(){
-      $('.icon_link_tab').addClass('open');
-    });
-    $('.close_btn').click(function(){
-      $('.icon_link_tab').removeClass('open');
-    });
-  });
+// $(document).on('turbolinks:load', function(){
+//   $(function(){
+//     $('#menu_link').click(function(){
+//       $('.icon_link_tab').addClass('open');
+//     });
+//     $('.close_btn').click(function(){
+//       $('.icon_link_tab').removeClass('open');
+//     });
+//   });
 
-  $(function(){
-    $('a[href^=#]').click(function() {
-      $('.icon_link_tab').removeClass('open');
-    });
-  });
-});
+//   $(function(){
+//     $('a[href^=#]').click(function() {
+//       $('.icon_link_tab').removeClass('open');
+//     });
+//   });
+// });
